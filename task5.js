6c249172b73693cbcc1bca65d86bf61420a288d0
AnastasiiaIvanets <116296326+AnastasiiaIvanets@users.noreply.github.com>
2022-10-21 20:29:59 +0000

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,7 @@
+function sorted_list_of_friends(friendList) {
+    return friendList.toUpperCase().split(";").map(item => {
+        let new_item = item.split(":");
+        return (`${new_item[1]}, ${new_item[0]}`)
+    }).sort().join("   ")
+}
+console.log(sorted_list_of_friends("Fired: Corwill; Wilfred: Corwill; Barney: TornBull; Betty: Tornbull; Bjon: Tornbull; Raphael: Corwill; Alfred: Corwill"))
